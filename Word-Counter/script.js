9f65d101e36ee5d9c4ff5b8ae8fbb49811f42390
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-08-23 17:58:46 +0530

    word limit feature completed

--- Word-Counter/script.js
+++ Word-Counter/script.js
@@ -1,5 +1,6 @@
-let ignore = false;
-let wordLimit = 5;
+let ignoreWordLimitWarning = false;
+let ignoreCharacterLimitWarning = false;
+let wordLimit = 0;
 
 setInterval(() => {
 	let sentence = document.getElementById('main-input').value;
@@ -15,8 +16,12 @@ setInterval(() => {
 		wordsLen -= 1;
 	}
 
-	if (wordsLen > wordLimit && ignore == false) {
-		enableWarning();
+	if (wordsLen > wordLimit && ignoreWordLimitWarning == false) {
+		enableWordLimit();
+	}
+
+	if (sentence.length > 10 && ignoreCharacterLimitWarning == false) {
+		//enableCharLimit();
 	}
 
 	console.log(wordsLen);
@@ -25,24 +30,48 @@ setInterval(() => {
 	document.getElementById('word-lim').innerHTML = `Word Limit: ${wordLimit}`;
 }, 100);
 
-let enableWarning = () => {
-	document.getElementById('overlay').style.display = 'flex';
-	document.getElementById('main-input').maxLength = 0;
-};
-
-let disableWarning = () => {
-	document.getElementById('overlay').style.display = 'none';
-	ignore = true;
-	document.getElementById('main-input').maxLength = 524288; //default
-};
+//WORD LIMIT FUNCTIONS
 
 let enableSetWordLimit = () => {
-	console.log('set limit window enabled');
+	console.log('set word limit window enabled');
 	document.getElementById('set-word-limit').style.display = 'flex';
 };
 
-let setWordLimit = () => {
+let submitWordLimit = () => {
 	console.log(document.getElementById('word-limit-input').value);
 	wordLimit = document.getElementById('word-limit-input').value;
 	document.getElementById('set-word-limit').style.display = 'none';
+};
+
+let enableWordLimit = () => {
+	document.getElementById('word-limit-overlay').style.display = 'flex';
+	document.getElementById('main-input').maxLength = 0;
+};
+
+let disableWordLimitWarning = () => {
+	document.getElementById('word-limit-overlay').style.display = 'none';
+	ignoreWordLimitWarning = true;
+	document.getElementById('main-input').maxLength = 524288; //default
+};
+
+// WORD LIMIT FUNCTIONS ENDS
+
+let enableSetCharLimit = () => {
+	console.log('set character limit window enabled');
+	document.getElementById('set-char-limit').style.display = 'flex';
+};
+
+let enableCharLimit = () => {
+	console.log('character limit is enabled');
+	document.getElementById('main-input').maxLength = 0;
+	document.getElementById('char-limit-overlay').style.display = 'flex';
+};
+
+let setCharacterLimit = () => {
+	console.log('character limit set');
+};
+
+let disableCharLimitWarning = () => {
+	document.getElementById('char-limit-overlay').style.display = 'none';
+	document.getElementById('main-input').maxLength = 524288; //default
 };

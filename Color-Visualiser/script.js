0a6185616c8b974026bf96071b6cd495c0108a69
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-08-24 22:32:10 +0530

    hex code conversion logic complete

--- Color-Visualiser/script.js
+++ Color-Visualiser/script.js
@@ -1,31 +1,92 @@
-let bck = document.getElementById('background');
-let rgb = document.getElementById('rgb');
+let bckGround = document.getElementById('background');
+let rgbBtn = document.getElementById('rgb');
+let hexBtn = document.getElementById('hex');
 let sliderRed = document.getElementById('slider-red');
 let sliderGreen = document.getElementById('slider-green');
 let sliderBlue = document.getElementById('slider-blue');
 
-let Color;
+let rgbString, hexString;
+let r, g, b;
 
 function updateColours() {
-	let r = sliderRed.value;
-	let g = sliderGreen.value;
-	let b = sliderBlue.value;
-	Color = `rgb(${r},${g},${b})`;
-	rgb.innerHTML = Color;
-	bck.style.backgroundColor = Color;
+	r = sliderRed.value;
+	g = sliderGreen.value;
+	b = sliderBlue.value;
+
+	rgbString = rgbColor();
+	hexString = hexColor();
+
+	rgbBtn.innerHTML = rgbString;
+	hexBtn.innerHTML = hexString;
+
+	bckGround.style.backgroundColor = rgbString;
 }
+
+let rgbColor = () => {
+	return `rgb(${r},${g},${b})`;
+};
+
+updateColours();
 
 sliderRed.addEventListener('input', updateColours);
 sliderGreen.addEventListener('input', updateColours);
 sliderBlue.addEventListener('input', updateColours);
 
-const copyToClipboard = () => {
+const copyRGBToClipboard = () => {
 	const el = document.createElement('textarea');
-	el.value = Color;
+	el.value = rgbString;
+	el.setAttribute('readonly', '');
+	el.style.position = 'absolute';
+	el.style.left = '-9999px';
 	document.body.appendChild(el);
 	el.select();
 	document.execCommand('copy');
 	document.body.removeChild(el);
 };
 
-document.getElementById('rgb').addEventListener('click', copyToClipboard);
+const copyHEXToClipboard = () => {
+	const el = document.createElement('textarea');
+	el.value = hexString;
+	el.setAttribute('readonly', '');
+	el.style.position = 'absolute';
+	el.style.left = '-9999px';
+	document.body.appendChild(el);
+	el.select();
+	document.execCommand('copy');
+	document.body.removeChild(el);
+};
+
+rgbBtn.addEventListener('click', copyRGBToClipboard);
+hexBtn.addEventListener('click', copyHEXToClipboard);
+
+function hexColor() {
+	let mAp = ['a', 'b', 'c', 'd', 'e', 'f'];
+	let char1;
+	let char2;
+	let flag = 0;
+	let colr = r;
+	let mainString = '';
+
+	function convert(char) {
+		let finalChar;
+		char > 9 ? (finalChar = mAp[char - 10]) : (finalChar = `${char}`);
+		return finalChar;
+	}
+
+	for (let i = 0; i < 6; i++) {
+		i > 1 && i < 4 ? (colr = g) : i > 3 ? (colr = b) : console.log('loop end');
+
+		char1 = Math.floor(colr / 16);
+		char2 = (colr / 16 - char1) * 16;
+
+		if (flag == 0) {
+			mainString += convert(char1);
+			flag = 1;
+		} else {
+			mainString += convert(char2);
+			flag = 0;
+		}
+	}
+	console.log(mainString);
+	return `#${mainString}`;
+}

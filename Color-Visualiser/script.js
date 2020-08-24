d7c62423ca23a8b719c6453007a459f21185dc4c
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-08-24 13:21:26 +0530

    logic update

--- Color-Visualiser/script.js
+++ Color-Visualiser/script.js
@@ -1,3 +1,29 @@
 let bck = document.getElementById('background');
+let rgb = document.getElementById('rgb');
+let sliderRed = document.getElementById('slider-red');
+let sliderGreen = document.getElementById('slider-green');
+let sliderBlue = document.getElementById('slider-blue');
 
-bck.style.backgroundColor = 'red';
+let r = 40,
+	g = 0,
+	b = 0,
+	a = 0;
+
+function updateColours() {
+	let r = sliderRed.value;
+	let g = sliderGreen.value;
+	let b = sliderBlue.value;
+	rgb.innerHTML = `rgb(${r},${g},${b})`;
+	bck.style.backgroundColor = `rgb(${r},${g},${b})`;
+}
+
+sliderRed.addEventListener('input', updateColours);
+sliderGreen.addEventListener('input', updateColours);
+sliderBlue.addEventListener('input', updateColours);
+
+/*
+sliderBlue.oninput = () => {
+	b = sliderGreen.value;
+	bck.style.backgroundColor = `rgb(${r},${g},${b})`;
+};
+*/

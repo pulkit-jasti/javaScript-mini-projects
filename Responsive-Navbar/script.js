8680dc26e4ede5c4c7c73ba7939b4a9918bfcf8a
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-01 00:52:19 +0530

    final update

--- Responsive-Navbar/script.js
+++ Responsive-Navbar/script.js
@@ -3,3 +3,10 @@ $('.menu-btn').click(function () {
 	$('.overlay').toggleClass('active');
 	$('.menu-container').toggleClass('active');
 });
+
+const cursor = curDot();
+
+cursor.over('.line-1', {
+	borderColor: 'rgba(255,255,255,.38)',
+	broderWidth: 2,
+});

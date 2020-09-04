b9991f434fdb1636c345a2c435ad14e7b54ee18f
pulkit-jasti <31801730+pulkit-jasti@users.noreply.github.com>
2020-09-04 18:37:36 +0530

    reset button logic completed

--- Simple Photo Editor/script.js
+++ Simple Photo Editor/script.js
@@ -26,3 +26,29 @@ function updateValues() {
 sliders.forEach(element => {
 	element.addEventListener('click', updateValues);
 });
+
+document.getElementById('reset').addEventListener('click', () => {
+	document.getElementById('grayscale').value = 0;
+	document.getElementById('blur').value = 0;
+	document.getElementById('brightness').value = 100;
+	document.getElementById('contrast').value = 100;
+	document.getElementById('hue').value = 0;
+	document.getElementById('opacity').value = 100;
+	document.getElementById('color-invert').value = 0;
+	document.getElementById('saturate').value = 100;
+	document.getElementById('sepia').value = 0;
+
+	grayscale = document.getElementById('grayscale').value;
+	Blur = document.getElementById('blur').value;
+	brightness = document.getElementById('brightness').value;
+	contrast = document.getElementById('contrast').value;
+	hue = document.getElementById('hue').value;
+	opacity = document.getElementById('opacity').value;
+	colorInvert = document.getElementById('color-invert').value;
+	saturate = document.getElementById('saturate').value;
+	sepia = document.getElementById('sepia').value;
+
+	image.style.filter = `grayscale(${grayscale}%) blur(${Blur}px) brightness(${brightness}%) contrast(${contrast}%) hue-rotate(${hue}deg) opacity(${opacity}%) invert(${colorInvert}%) saturate(${saturate}%) sepia(${sepia}%) `;
+
+	console.log('reset');
+});
